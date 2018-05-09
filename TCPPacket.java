921925fa91b7604cd81f5260afbb9e28ef8dc4ed
Rishabh Khandelwal <rkhandelwal@Rishabh.eau.wi.charter.com>
2018-05-09 13:22:07 -0500

    pushing a new version of the code a

--- TCPPacket.java
+++ TCPPacket.java
@@ -29,13 +29,15 @@ public class TCPPacket {
 	}
 
 	public void setFlags(String flag) {
-		if (flag.contains("A")) this.length += 1;
-		if (flag.contains("F")) this.length += 2;
-		if (flag.contains("S")) this.length += 4;
+		int len = Integer.reverse(this.length);
+		if (flag.contains("A")) len += 1;
+		if (flag.contains("F")) len += 2;
+		if (flag.contains("S")) len += 4;
+		this.length = Integer.reverse(len);
 	}
 	
 	public String getFlags() {
-		int len = this.length;
+		int len = Integer.reverse(this.length);
 		int relBits = len % 8; //get the relevant bits
 		String flag = "";
 		switch(relBits){
@@ -157,7 +159,7 @@ public class TCPPacket {
         byteBuffer.putInt(this.seq);                     //index: 0 - 3
         byteBuffer.putInt(this.ack);               //index: 4 - 7
         byteBuffer.putLong(this.timsStamp);                   //index: 8 - 15
-        byteBuffer.putInt((this.length));      //index: 16 - 19
+        byteBuffer.putInt(Integer.reverse(this.length));      //index: 16 - 19
         short Zero = 0;                            //concat 16 bits of zeros
         byteBuffer.putShort(Zero);                         //index: 20 - 21
         this.checksum = computeChecksum(this.seq);
@@ -210,7 +212,7 @@ public class TCPPacket {
         this.seq = byteBuffer.getInt();
         this.ack= byteBuffer.getInt();
         this.timsStamp= byteBuffer.getLong();
-        this.length = byteBuffer.getInt();
+        this.length = Integer.reverse(byteBuffer.getInt());
         short zero = byteBuffer.getShort();
         assert(zero == 0);
         this.checksum = byteBuffer.getShort();
