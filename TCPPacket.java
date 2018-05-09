b42689b7b02990d0a968fb636d9358ca92f4f9bd
Rishabh Khandelwal <rkhandelwal@Rishabh.eau.wi.charter.com>
2018-05-09 13:01:12 -0500

    pushing a new version of the code a

--- TCPPacket.java
+++ TCPPacket.java
@@ -201,7 +201,7 @@ public class TCPPacket {
     public TCPPacket deserialize(byte[] recUnit) {
     	
     	//TCPPacket packet = new TCPPacket();
-    	
+    	System.out.println("In the deserialize function");
     	ByteBuffer byteBuffer = ByteBuffer.wrap(recUnit, 0, recUnit.length);
     		if (byteBuffer == null) {
     			System.out.println("No data in buffer");
